e0ce91a7a207022a8084b2f24423ef13d67666ec
tranvuluan2270 <tranvuluan2270@gmail.com>
2023-07-18 00:06:35 +0700

    ui templates

--- src/components/TopButton.jsx
+++ src/components/TopButton.jsx
@@ -1,5 +1,38 @@
 const TopButtons = () => {
-  return <div>Top Buttons</div>;
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 2,
+      title: "Sydney",
+    },
+    {
+      id: 3,
+      title: "Tokyo",
+    },
+    {
+      id: 4,
+      title: "Toronto",
+    },
+    {
+      id: 5,
+      title: "Paris",
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => {
+        return (
+          <button key={city.id} className="text-white text-lg font-medium">
+            {city.title}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default TopButtons;
